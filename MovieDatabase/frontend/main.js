a819048b7b3dd6ce80c9c62b22c5fd061de76d32
Ananyobrata Pal <ananyo141@gmail.com>
2022-06-30 14:21:20 +0530

    fix table showing get movies

--- MovieDatabase/frontend/main.js
+++ MovieDatabase/frontend/main.js
@@ -1,27 +1,88 @@
 let getall = async () => {
   let username = document.getElementById("userinput").value;
-  if (username) 
-    username += "/";
+  if (username) username += "/";
   let response = await fetch(`http://127.0.0.1:8000/${username}`);
   let movies = await response.json();
   console.log("Response: ", JSON.stringify(movies));
   movies = movies.filter((e) => e.user);
 
-  let li = `<table><tr class="bg-gray-200 p-8"><th>Name</th><th>Year</th></tr>`;
+  let count = 0;
+  let table = `
+  <div class="flex flex-col">
+  <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+    <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+      <div class="overflow-hidden">
+        <table class="min-w-full">
+          <thead class="border-b">
+            <tr>
+              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                #
+              </th>
+              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+              Name
+              </th>
+              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+              Director
+              </th>
+              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+              Year
+              </th>
+    
+              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+              Watched
+              </th>
+              
+              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+              Favourite
+              </th>
+    
 
-  // Loop through each data and add a table row
+              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+              Watchlist
+              </th>
+              
+            </tr>
+
+          </thead>
+          <tbody>
+    `;
+
   movies.forEach((movie) => {
-    li += `<tr>
-                <td>${movie.name} </td>
-                <td>${movie.director}</td>        
-                <td>${movie.year}</td>        
-                <td>${movie.is_watched}</td>        
-                <td>${movie.is_favourite}</td>        
-                <td>${movie.on_watchlist}</td>        
-            </tr>`;
+    count++;
+  table += `
+            <tr class="bg-white border-b">
+              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">${count}</td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+              ${movie.name}
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+              ${movie.director}
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+              ${movie.year}
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+              ${movie.is_watched}
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+              ${movie.is_favourite}
+              </td>
+              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+              ${movie.on_watchlist}
+              </td>
+            </tr>
+`;
   });
-  li += `</table>`;
+
+  table += `
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+  `;
 
   // Display result
-  document.getElementById("user-movies-show").innerHTML = li;
+  document.getElementById("user-movies-show").innerHTML = table;
 };

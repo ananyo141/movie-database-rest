59061ce2f5907f6c5d0437efd35055733dbffe53
Ananyobrata Pal <ananyo141@gmail.com>
2022-07-01 00:22:12 +0530

    remove unnecessary userserializer normalize username to lowercase return null response if user doesn't exists in getDataUser

--- MovieDatabase/api/serializers.py
+++ MovieDatabase/api/serializers.py
@@ -5,8 +5,3 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = '__all__'
--- MovieDatabase/api/views.py
+++ MovieDatabase/api/views.py
@@ -12,14 +12,18 @@ def getData(request):
 
 @api_view(['GET'])
 def getDataUser(request, pk):
-    userid = User.objects.get(username=pk)
+    try:
+        userid = User.objects.get(username=pk.lower())
+    except User.DoesNotExist:
+        return Response({})
+
     movies = userid.movie_set.all()
     serializer = MovieSerializer(movies, many=True)
     return Response(serializer.data)
 
 @api_view(['POST'])
 def addData(request):
-    user = User.objects.get_or_create(username=request.data['user'])[0]
+    user = User.objects.get_or_create(username=request.data['user'].lower())[0]
 
     request.POST._mutable = True
     request.data['user'] = user.id

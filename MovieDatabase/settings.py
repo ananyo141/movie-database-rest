e7c5e3525cc4b0415091e4f7abff1d2cce969657
Ananyobrata Pal <ananyo141@gmail.com>
2022-07-02 13:13:49 +0530

    revert

--- MovieDatabase/settings.py
+++ MovieDatabase/settings.py
@@ -81,18 +81,13 @@ WSGI_APPLICATION = 'MovieDatabase.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 import dj_database_url
-    
-try:
-    DATABASES = {
-            'default': dj_database_url.config(),
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-except:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 
 # Password validation
